0224a7821534c2a1d6af6c60d5b3f2140f6335d2
playitouch@163.com <playitouch@163.com>
2019-06-24 23:55:39 +0800

    提交

---
+++ 02_Demos/NHibernate-AspNetMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NHibernate_AspNetMvc.MvcApplication" Language="C#" %>
---
+++ 02_Demos/NHibernate-AspNetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NHibernate_AspNetMvc.Startup))]
+namespace NHibernate_AspNetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
