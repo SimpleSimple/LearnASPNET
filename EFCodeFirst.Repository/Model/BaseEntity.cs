89b7a8db3134a8606fd2d883b0ab553efd24bf86
playitouch@163.com <playitouch@163.com>
2019-06-26 23:30:15 +0800

    提交

---
+++ EFCodeFirst.Repository/Model/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace EFCodeFirst.Repository.Model
+{
+    public class BaseEntity<T>
+    {
+        public T ID { get; set; }
+    }
+}
---
+++ EFCodeFirst.Repository/Model/TableA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirst.Repository.Model
+{
+    public class TableA : BaseEntity<long>
+    {
+        public string Name { get; set; }
+
+        public string B_ID { get; set; }
+
+        public virtual List<TableB> TableBList { get; set; }
+    }
+}
